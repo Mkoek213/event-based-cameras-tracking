e7a048b6dd5c17e6ffa2d9d358a9121aa60cbbdf
Mikolaj PC <mikolajkolek@seedia.city>
2026-04-14 21:13:06 +0200

    removed unused files

--- src/models/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Models sub-package: backbone, detector, and tracker."""
-
-from .backbone import EventBackbone
-from .detector import EventDetector
-from .tracker import EventTracker
-
-__all__ = ["EventBackbone", "EventDetector", "EventTracker"]
